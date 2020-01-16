585efbf4bc06280d9a1b3417b9af77110f29af13
nlee15 <54679384+nlee15@users.noreply.github.com>
2020-01-16 14:41:31 -0600

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Nicolas lee Wen
+ * Date: 2019/08/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
