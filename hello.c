505b754da72d84d49d236827d57688ce33466ed9
rlinnemann <59941482+rlinnemann@users.noreply.github.com>
2020-01-16 15:13:06 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -13,5 +13,8 @@ int main(int argc, char **argv) {
   printf("Nicolas Lee Wen Han\n");
   printf("Computer Engineering\n");
 
+  printf("Robert Martin Linnemann\n");
+  printf("Electrical Engineering\n");
+  
   return 0;
 }
